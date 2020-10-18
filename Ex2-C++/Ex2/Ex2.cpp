fecfaf89f9f08303deb31fd509f86900e8f3d560
Pamela Chaaya <pamelachaaya@gmail.com>
2020-10-18 17:32:00 +0300

    Fixing Ex2

--- Ex2-C++/Ex2/Ex2.cpp
+++ Ex2-C++/Ex2/Ex2.cpp
@@ -1,38 +1,49 @@
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
-#include <stddef.h>
-#include <stdio.h>
-
-using namespace cv;
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <stdlib.h>
 using namespace std;
-int main(int argc, char* argv[]) {
-	// ./filter -t <median|average> -i filename -w kernel
-	if (argc < 7) {
-		cout << "Invalid Command";
-		return -1;
-	}
-	string type;
-	string filename;
-	char kernel;
-	const char* n_argv[] = { "./filter","-t",type,"-i",filename, "-w",kernel,NULL };
-	argv = n_argv;
-	string a = argv[0];
-	string b = argv[1];
-	string c = argv[3];
-	string d = argv[5];
-	int k = argv[6];
-	string nameimg= argv[4];
-	string blurtype = argv[2];
-	Mat img, output;
-	if (blurtype.compare("median") == 0 || blurtype.compare("average")== 0) {
-		img = imread(nameimg, IMREAD_ANYCOLOR | IMREAD_ANYDEPTH);
-		medianBlur(img, output,k);
-		imwrite("my_image.out.png", output); 
-		namedWindow("Display Window", WINDOW_NORMAL);
-		imshow("Display Window", output);
-		waitKey(0);
-	}
-	return 0;
+using namespace cv;
+
+int main(int ac, char** av[]) {
+    
+    if (ac == 7) {
+        Mat image = imread(av[4], IMREAD_ANYCOLOR | IMREAD_ANYDEPTH); //reading colorful image
+        Mat output;
+        if (image.empty())
+        {
+            cout << "No image found" << endl;
+            return (0);
+        }
+        int k = strtol(av[6]);
+        string type = av[2];
+        switch (type) {
+        case median:
+            // apply a median filter:
+            medianBlur(image, output, k);
+            break;
+        case mean:
+            // apply average filter:
+            blur(image, output,size(k, k));
+            break;
+        default:
+            cout << "Type of filter not found" << endl;
+
+        }
+
+        imwrite("my_image.out.png", output);
+        namedWindow("Display Window", WINDOW_NORMAL);
+        imshow("Display Window", output);
+        
+        waitKey(0);
+    }
+    else {
+        cout << "Wrong Command" << endl;
+    }
+
+    return 0;
 }
